83a45d6fedcbea4202406a5f01dce3d34410063b
z0glen <zgbauer@gmail.com>
2019-02-02 20:10:19 -0500

    fix(index) update file paths

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const app = express()
 const path = require('path');
 const port = 7845
 
-app.use(express.static('public'))
+app.use(express.static('mcsnacks19/public'))
 
 app.get('/hi', (req, res) => res.send('Hello World!'))
-app.get('/hello', (req, res) => res.sendFile(path.join(__dirname+'/public/hello.html')))
-app.get('/pickle', (req, res) => res.sendFile(path.join(__dirname+'/public/pickel.html')))
+app.get('/hello', (req, res) => res.sendFile(path.join(__dirname+'/mcsnacks19/public/hello.html')))
+app.get('/pickle', (req, res) => res.sendFile(path.join(__dirname+'/mcsnacks19/public/pickel.html')))
 
 app.listen(process.env.PORT || port, function() {
 	console.log(`Example app listening on port ${port}!`);
