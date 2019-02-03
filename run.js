a83908a26b714411926d26d7f2de24bd4fa062b8
Seth Penna <smpenna3@gmail.com>
2019-02-02 22:41:05 -0500

    Added first level to map

--- run.js
+++ run.js
@@ -3,7 +3,8 @@ const app = express()
 const path = require('path');
 const port = 7845
 
-app.use(express.static('scrolling_map'))
+app.use(express.static('scrolling_map'));
+app.use(express.static('mcsnacks19/public'));
 
 app.get('/hi', (req, res) => res.send('Hello World!'))
 app.get('/map', (req, res) => res.sendFile(path.join(__dirname+'/scrolling_map/map.html')))
